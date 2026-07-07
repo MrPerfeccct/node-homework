3d721e07dfb783fe8c1d08084927457037cf153b
MrPerfeccct <1ammrperfeccct@gmail.com>
2026-07-07 18:54:13 -0500

    Complete lesson-1

--- assignment1/core-modules-demo.js
+++ assignment1/core-modules-demo.js
@@ -8,11 +8,24 @@ if (!fs.existsSync(sampleFilesDir)) {
 }
 
 // OS module
+console.log('Platform:', os.platform());
+console.log('CPU:', os.cpus()[0].model);
+console.log('Total Memory:', os.totalmem());
 
 
 // Path module
+const joinedPath = path.join(sampleFilesDir, 'folder', 'file.txt');
+console.log('Joined path:', joinedPath);
 
 // fs.promises API
+(async () => {
+  const demoPath = path.join(sampleFilesDir, 'demo.txt');
+
+  await fs.promises.writeFile(demoPath, 'Hello from fs.promises!');
+  const content = await fs.promises.readFile(demoPath, 'utf8');
+
+  console.log('fs.promises read:', content);
+})();
 
 
 // Streams for large files- log first 40 chars of each chunk
